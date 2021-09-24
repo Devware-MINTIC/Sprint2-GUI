7000d18ea5665c0508a7897b3853cd13b03f55f5
Luis Felipe López <luis.lopez@itconsultores.com.co>
2021-09-23 19:26:27 -0500

    [FEATURE] create user management UI

---
+++ UserManagement/users.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    "id": "1",
+    "username": "Carlos",
+    "role": "VENDEDOR",
+    "lastConnection": "2021/09/23 06:52:23"
+  },
+  {
+    "id": "2",
+    "username": "Marta",
+    "role": "VENDEDOR",
+    "lastConnection": "2021/09/23 06:52:23"
+  },
+    {
+    "id": "3",
+    "username": "Felipe",
+    "role": "CLIENTE",
+    "lastConnection": "2021/09/23 06:52:23"
+  },
+    {
+    "id": "4",
+    "username": "Juan",
+    "role": "CLIENTE",
+    "lastConnection": "2021/09/23 06:52:23"
+  },
+    {
+    "id": "5",
+    "username": "Julian",
+    "role": "VENDEDOR",
+    "lastConnection": "2021/09/23 06:52:23"
+  }
+]
