4c3b99a04ee8749f3b740c2a8d2459ddd8d1093d
Katherine Arias <katherinearias0526@gmail.com>
2021-10-16 09:09:06 -0500

    cambio vista products

--- src/views/Products.js
+++ src/views/Products.js
@@ -1,153 +1,195 @@
-import React, { useEffect, useState } from 'react';
-import '../components/ComponentsProducts/products.css';
-import Header from '../components/ComponentsProducts/Header';
-import Listing from '../components/ComponentsProducts/Listing';
-import { getProducts } from '../services';
+import React, { useEffect, useState, forwardRef, useCallback } from "react";
 
-function Products() {
-  const initialState = [
-    {
-      title: 'Traje Banana',
-      location: 'Disponible',
-      price: 50000,
-      id: '107',
-    },
-    {
-      title: 'Traje Champions',
-      location: 'No Disponible',
-      price: 37000,
-      id: '108',
-    },
-    {
-      title: 'Traje Lluvia',
-      location: 'Disponible',
-      price: 32000,
-      id: '109',
-    },
-    {
-      title: 'Traje Bailarina',
-      location: 'Disponible',
-      price: 45000,
-      id: '106',
-    },
-  ];
+import MaterialTable from "material-table";
+import ArrowDownward from "@material-ui/icons/ArrowDownward";
+import Check from "@material-ui/icons/Check";
+import ChevronLeft from "@material-ui/icons/ChevronLeft";
+import ChevronRight from "@material-ui/icons/ChevronRight";
+import Clear from "@material-ui/icons/Clear";
+import Edit from "@material-ui/icons/Edit";
+import FirstPage from "@material-ui/icons/FirstPage";
+import LastPage from "@material-ui/icons/LastPage";
+import Search from "@material-ui/icons/Search";
 
-  const [query, setQuery] = useState('');
-  const [listings, setListings] = useState(initialState);
-  const [filteredListings, setFilteredListings] = useState([]);
-  const [isFiltered, setIsFiltered] = useState(false);
-  const [isEdit, setIsEdit] = useState(false);
-  const [title, setTitle] = useState('');
-  const [price, setPrice] = useState('');
-  const [location, setLocation] = useState('');
-  const [currentListing, setCurrentListing] = useState({});
-  const currentListings = isFiltered ? filteredListings : listings;
+import useAuth from "../hooks/useAuth"
 
-  const handleQueryChange = (e) => {
-    if (e.target.value) {
-      setIsFiltered(true);
-    } else setIsFiltered(false);
+import { getProducts, updateProductById, createProduct } from "../services/products";
 
-    setQuery(e.target.value);
-    // filter(e.target.value);
-  };
+const tableIcons = {
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => (
+    <ChevronLeft {...props} ref={ref} />
+  )),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+};
 
-  const handleEditClick = (e) => {
-    const item = listings.filter((listing) => listing.id === e.target.value);
-    setIsEdit(true);
-    setCurrentListing(item[0]);
-    setTitle(item[0].title);
-    setPrice(item[0].price);
-    setLocation(item[0].location);
-  };
+const Products = () => {
+  const { setIsLoading } = useAuth();
+  const [dataProducts, setDataProducts] = useState([]);
+  const [name, setName] = useState("");
+  const [value, setValue] = useState("");
+  const [status, setStatus] = useState(true);
 
-  const handleDeleteClick = (e) => {
-    const newListings = listings.filter(
-      (listing) => listing.id !== e.target.value
-    );
-    setListings(newListings);
-  };
-
-  const clearInput = () => {
-    setIsEdit(false);
-    setTitle('');
-    setPrice('');
-    setLocation('');
-    setCurrentListing({});
-  };
-
-  const handleAddOrEditClick = () => {
-    if (isEdit) {
-      const newListing = {
-        title,
-        location,
-        price: +price,
-        id: currentListing.id,
-      };
-      editCurrentListing(newListing);
-    } else {
-      const newListing = {
-        title,
-        location,
-        price: +price,
-        id: Math.floor(Math.random() * 101).toLocaleString(),
-      };
-      addListing(newListing);
-    }
-    clearInput();
-  };
-
-  const editCurrentListing = (newListing) => {
-    setListings(
-      listings.map((listing) =>
-        listing.id === currentListing.id ? newListing : listing
-      )
-    );
-  };
-
-  const addListing = (newListing) => {
-    setListings([newListing, ...listings]);
-  };
-  useEffect(() => {
-    if (query) {
-      const filteredListings = listings.filter(
-        (listing) => listing.location === query
-      );
-      setFilteredListings(filteredListings);
-    } else setFilteredListings([]);
-  }, [listings, query]);
+  const isSaleValid =
+  dataProducts?.some((product) => product.value > 0) &&
+  !!name &&
+  !!status;
 
   useEffect(() => {
+    setIsLoading(true);
     getProducts().then((data) => {
-      console.log("PRODUCTS", data);
-    });
+      setDataProducts(data?.products);
+    }).finally(() => setIsLoading(false));
   }, []);
 
+  const columns = [
+    { title: "uid", field: "uid", hidden: true, editable: "never" },
+    { 
+      title: "Nombre del producto", 
+      field: "name", 
+      editable: "onUpdate" 
+    },
+    { 
+      title: "Precio", 
+      field: "value", 
+      editable: "onUpdate",
+      type: "numeric",
+      validate: (rowData) => rowData.value >= 0,
+    },
+    {
+      title: "Disponibilidad",
+      field: "state",
+      editable: "onUpdate",
+      type: "boolean",
+      
+    },
+    
+   
+  ];
+
+  const handleRowUpdate = (newData, oldData, resolve) => {
+    setIsLoading(true);
+    updateProductById({
+      uid: newData.uid,
+      state: newData.state,
+    }).then(() => {
+      const dataUpdate = [...dataProducts];
+      const index = oldData.tableData.id;
+      dataUpdate[index] = newData;
+      setDataProducts([...dataUpdate]);
+      resolve();
+    }).finally(() => setIsLoading(false));
+  };
+  const handleCreateProduct = () => {
+    setIsLoading(true);
+    const products = dataProducts
+      .filter((product) => product.value > 0)
+      .map((product) => {
+        const { tableData, state, ...rest } = product;
+        return rest;
+      });
+    createProduct({
+      name,
+      value,
+      status,
+    }).then((response) => {
+      setName("");
+      setValue();
+      setStatus(true);
+
+    }).finally(() => setIsLoading(false));
+  };
+
+  const getAllProducts = useCallback(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      getProducts().then((data) => {
+        const newProducts = data?.products
+          .filter((product) => product.state)
+          .map((product) => ({
+            ...product,
+            value: 0,
+          }));
+        setDataProducts(newProducts);
+      }).finally(() => setIsLoading(false));
+    }, 200)
+    
+  }, [setIsLoading]);
+
+  useEffect(() => {
+    getAllProducts();
+  }, [getAllProducts]);
+   
+
   return (
-    <div className="products">
-      <Header
-        query={query}
-        handleQueryChange={handleQueryChange}
-        isEdit={isEdit}
-        title={title}
-        setTitle={setTitle}
-        price={price}
-        setPrice={setPrice}
-        location={location}
-        setLocation={setLocation}
-        clearInput={clearInput}
-        handleAddOrEditClick={handleAddOrEditClick}
+    <div className="container">
+      <div class="my-3 row">
+        <div class="col">
+          <label>Nombre del producto</label>
+          <input
+            type="text"
+            class="form-control"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div class="col">
+          <label>Precio del producto</label>
+          <input
+            type="number"
+            class="form-control"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+        </div>
+        <div class="col">
+          <label>Disponibilidad</label>
+          <input
+            type="boolean"
+            class="form-control"
+            value={status}
+            onChange={(e) => setStatus(e.target.value)}
+          />
+        </div>
+      </div>
+      <MaterialTable
+        title=""
+        columns={columns}
+        data={dataProducts}
+        icons={tableIcons}
+        editable={{
+          onRowUpdate: (newData, oldData) =>
+            new Promise((resolve) => {
+              handleRowUpdate(newData, oldData, resolve);
+            }),
+        }}
+        options={{
+          actionsColumnIndex: -1,
+        }}
+        style={{
+          display: "grid",
+          height: "350px",
+        }}
       />
-      {currentListings.length === 0 && (
-        <h2 style={{ paddingTop: 10, textAlign: 'center' }}>No Results</h2>
-      )}
-      <Listing
-        listings={currentListings}
-        handleEditClick={handleEditClick}
-        handleDeleteClick={handleDeleteClick}
-      />
+      <div className="d-flex justify-content-between mt-5">
+         <button
+          disabled={!isSaleValid}
+          className="btn btn-success px-5"
+          onClick={handleCreateProduct}
+        >
+          Crear Producto
+        </button>
+      </div>
     </div>
   );
-}
+};
+
 
 export default Products;
